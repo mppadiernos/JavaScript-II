a2fd627d3087c52f35004486dab7cebb05ae2da3
Michael Padiernos <m.padiernos@outlook.com>
2019-10-05 23:20:24 -0400

    finished MVP

--- assignments/array-methods.js
+++ assignments/array-methods.js
@@ -58,7 +58,7 @@ const runners = [
 // ==== Challenge 1: Use .forEach() ====
 // The event director needs both the first and last names of each runner for their running bibs. Combine both the first and last names and populate a new array called `fullNames`. This array will contain just strings.
 let fullNames = [];
-let  nameRunners = runners.forEach(function(name) {
+runners.forEach(function(name) {
   fullNames.push(`${name.first_name} ${name.last_name}`);
 });
 console.log(fullNames);
@@ -66,25 +66,23 @@ console.log(fullNames);
 // ==== Challenge 2: Use .map() ====
 // The event director needs to have all the runners' first names in uppercase because the director BECAME DRUNK WITH POWER. Populate an array called `firstNamesAllCaps`. This array will contain just strings.
 let firstNamesAllCaps = runners.map(function(firstName){
-  let str = firstName.first_name;
-  return str.toUpperCase(firstName.first_name);
+  let caps = firstName.first_name;
+  return caps.toUpperCase(firstName.first_name);
 });
-
 console.log(firstNamesAllCaps);
 
 // ==== Challenge 3: Use .filter() ====
 // The large shirts won't be available for the event due to an ordering issue. We need a filtered version of the runners array, containing only those runners with large sized shirts so they can choose a different size. This will be an array of objects.
-let runnersLargeSizeShirt = [];
-let shirtSize = runners.filter(function (size) {
-  if(size === 'large') {
-    return true;
-  }
+let runnersLargeSizeShirt = runners.filter((size) => {
+  return size.shirt_size === 'L';
 });
 console.log(runnersLargeSizeShirt);
 
 // ==== Challenge 4: Use .reduce() ====
 // The donations need to be tallied up and reported for tax purposes. Add up all the donations and save the total into a ticketPriceTotal variable.
-let ticketPriceTotal = 0;
+let ticketPriceTotal = runners.reduce((total, price) => {
+  return total + price.donation;
+},0);
 console.log(ticketPriceTotal);
 
 // ==== Challenge 5: Be Creative ====
--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -73,6 +73,7 @@ multiplyNums(4, 4, multiplied => {
 function contains(item, list, cb) {
   // contains checks if an item is present inside of the given array/list.
   // Pass true to the callback if it is, otherwise pass false.
+  // return cb(list.includes(item));
     return cb(list.indexOf(item)!==item);
 }
 contains('Gum', items, inside => {
